e2b52b27a3ea60bcf0cafc9a92b01692749363f0
AlexandrePuiseux65 <alexandrepsandbox@gmail.com>
2023-05-29 04:06:12 +0200

    test non fini nav bar / alex

--- bin/_nav.php
+++ bin/_nav.php
@@ -1,13 +1,26 @@
 <ul id="nav">
-    <?php if (!isset($_SESSION['utilisateur'])) { ?>
+    <?php if (!isset($_SESSION['utilisateur']) && !isset($_SESSION['administrateur'])) { ?>
         <div class="nav-logo">
             <li><a href="index.php"><img src="img/logo1.png" /></a></li>
         </div>
         <div class="nav-links">
-            <li><a href="Product.php"> Nos Produits </a></li>
+            <li><a href="Product.php">Nos Produits</a></li>
             <li><a href="aboutus.php">À propos</a></li>
             <li><a href="register.php">Créer un compte</a></li>
             <li><a href="login.php">Connexion</a></li>
+            <li><a href="shopping.php"><img src="img/panier.png" /></a></li>
+        </div>
+
+    <?php } elseif (isset($_SESSION['utilisateur']) && !isset($_SESSION['administrateur'])) { ?>
+
+        <div class="nav-logo">
+            <li><a href="index.php"><img src="img/logo1.png" /></a></li>
+        </div>
+        <div class="nav-links">
+            <li><a href="Product.php">Produits</a></li>
+            <li><a href="aboutus.php">À propos</a></li>
+            <li><a href="user.php"><?php echo $_SESSION['utilisateur']['Adresse_email'] ?></a></li>
+            <li><a href="logout.php">Déconnexion</a></li>
             <li><a href="shopping.php"><img src="img/panier.png" /></a></li>
         </div>
 
@@ -21,21 +34,6 @@
             <li><a href="add_product.php">Ajouter un produit</a></li>
             <li><a href="remove_product.php">Retirer un produit</a></li>
             <li><a href="logout.php">Déconnexion</a></li>
-        </div>
-
-    <?php } else { ?>
-
-        <div class="nav-logo">
-            <li><a href="index.php"><img src="img/logo1.png" /></a></li>
-        </div>
-        <div class="nav-links">
-            <li><a href="Product.php">Produits</a></li>
-            <li><a href="aboutus.php">À propos</a></li>
-            <li><a href="user.php">
-                    <?php echo $_SESSION['utilisateur']['Adresse_email'] ?>
-                </a></li>
-            <li><a href="logout.php">Déconnexion</a></li>
-            <li><a href="shopping.php"><img src="img/panier.png" /></a></li>
         </div>
 
     <?php } ?>
