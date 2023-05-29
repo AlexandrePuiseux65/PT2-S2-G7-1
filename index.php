1a9a98e315bfeaeb84221e1e56b0fa50d0746b6b
Martin <charpmartin@gmail.com>
2023-05-29 08:57:50 +0200

    Added content and style on index.php /Martin

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
-<?php require_once('bin/_header.php');
-?>
+<?php require_once('bin/_header.php');?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
+<div class="index-main">
 
-<body>
-    <h1>Page d'accueil</h1>
-    <?php
-    require_once('bin/_footer.php');
-    ?>
-</body>
+    <div class="index-main-haut-de-page">
+        
+        <h1>La photo parfaite ne prend qu'un instant</h1>
+    
+    </div>
 
+    <h2 class="index-main-h2">Dernier article</h2>
+
+    <div class="index-main-content">
+        
+        <img src="https://fr.shopping.rakuten.com/photo/canon-eos-r-rf-24-105mm-f-4-7-1-is-stm-1575424850_ML.jpg" alt="">
+        <div class="index-main-content-element1">
+            <h3>Canon EOS R</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Veritatis vitae quae placeat vel dolorum eaque laboriosam quos animi 
+                voluptas dolore, sint illum deleniti ratione aut impedit a, delectus dolor! 
+                Totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, debitis rem eaque exercitationem perferendis laudantium repudiandae excepturi sit quas animi!
+            </p>
+            <a href="">En savoir plus</a>
+            <p class="index-main-content-element1-price">1099,00€</p>
+       </div> 
+       
+    </div>
+
+</div>
+
+
+<?php require_once('bin/_footer.php');?>
 </html>
